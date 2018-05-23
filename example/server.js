91a4b670f1a5940e1f14eb480b21b7b254101eef
Laptseu <alexlaptseu1@Alexs-MacBook-Pro.local>
2018-05-23 11:07:26 +1000

    voyager with authentiocation

--- example/server.js
+++ example/server.js
@@ -1,9 +1,18 @@
 const express = require('express');
 const graphqlHTTP = require('express-graphql');
+const cors = require('cors');
 
 const schema = require('./schema');
 
 const app = express();
+
+// const corsOptions = {
+//   origin: 'http://localhost:56325',
+//   credentials: true,
+
+// }
+
+app.use(cors());
 app.use(express.static(__dirname));
 app.use('/graphql', graphqlHTTP(() => ({ schema })));
 
